df89f3633a4c39ac0f860f63e5037f7ac69130f1
Evan Bacon <baconbrix@gmail.com>
2018-01-27 16:47:35 -0800

    Added daily reward and basic currency firebase logic

--- ExpoParty/Fire.js
+++ ExpoParty/Fire.js
@@ -6,6 +6,8 @@ import Secret from './Secret';
 import getUserInfo from './utils/getUserInfo';
 import getSlug from './utils/getSlug';
 
+const collectionName = 'test-' + getSlug(); //users
+
 class Fire {
   constructor() {
     this.init();
@@ -17,7 +19,7 @@ class Fire {
   observeAuth = () =>
     firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
 
-  onAuthStateChanged = user => {
+  onAuthStateChanged = async user => {
     if (!user) {
       try {
         firebase.auth().signInAnonymously();
@@ -25,7 +27,103 @@ class Fire {
         alert(message);
       }
     } else {
+      await this.getUser();
       this.saveUser();
+      this.compareDaily();
+    }
+  };
+
+  compareDaily = async () => {
+    const dailyVisits = await this.incrementDailyReward();
+    if (dailyVisits != this.userData.dailyVisits) {
+      console.log('Yay! You came back, your streak is now at: ' + dailyVisits);
+      this.userData.dailyVisits = dailyVisits;
+      /// Give reward!
+    }
+  };
+
+  incrementDailyReward = async () => {
+    const timestamp = new Date().getTime();
+    return new Promise((res, rej) => {
+      this.db
+        .runTransaction(transaction => {
+          return transaction.get(this.doc).then(userDoc => {
+            if (!userDoc.exists) {
+              throw 'Document does not exist!';
+            }
+
+            const data = userDoc.data();
+            const { lastRewardTimestamp } = data;
+
+            const hours = Math.abs(lastRewardTimestamp - timestamp) / 36e5;
+
+            if (hours >= 24) {
+              if (hours >= 48) {
+                // It has been more than 1 day since the last visit - break the streak
+                const newDailyVisits = 0;
+                transaction.update(this.doc, {
+                  dailyVisits: newDailyVisits,
+                  lastRewardTimestamp: timestamp,
+                });
+                this.user.lastRewardTimestamp = timestamp;
+                return newDailyVisits;
+              } else {
+                // Perfect! It has been 1 day since the last visit - increment streak and save current time
+
+                const dailyVisits = data.dailyVisits || 0;
+                const newDailyVisits = dailyVisits + 1;
+                transaction.update(this.doc, {
+                  dailyVisits: newDailyVisits,
+                  lastRewardTimestamp: timestamp,
+                });
+                this.user.lastRewardTimestamp = timestamp;
+                return newDailyVisits;
+              }
+            } else {
+              // It hasn't been a day yet - do nothing
+            }
+            return data.dailyVisits || 0;
+          });
+        })
+        .then(res)
+        .catch(rej);
+    });
+  };
+
+  addCurrency = amount => {
+    return new Promise((res, rej) => {
+      this.db
+        .runTransaction(transaction => {
+          return transaction.get(this.doc).then(userDoc => {
+            if (!userDoc.exists) {
+              throw 'Document does not exist!';
+            }
+
+            const data = userDoc.data();
+            const currency = data.currency || 0;
+            const newCurrency = currency + amount;
+            transaction.update(this.doc, { currency: newCurrency });
+            this.user.currency = newCurrency;
+            return newCurrency;
+          });
+        })
+        .then(res)
+        .catch(rej);
+    });
+  };
+
+  get collection() {
+    return this.db.collection(collectionName);
+  }
+  get doc() {
+    return this.collection.doc(this.uid);
+  }
+
+  getUser = async () => {
+    try {
+      this.userData = await this.doc.get();
+    } catch (error) {
+      console.warn(error);
     }
   };
 
@@ -39,9 +137,8 @@ class Fire {
     if (!uid) {
       return;
     }
-    console.log('saveUserInfo', uid, info);
-    const ref = this.db.collection('users').doc(uid);
-    const setWithMerge = ref.set({ uid, ...info }, { merge: true });
+    // console.log('saveUserInfo', uid, info);
+    const setWithMerge = this.doc.set({ uid, ...info }, { merge: true });
   };
 
   saveScore = score => {
@@ -54,8 +151,7 @@ class Fire {
   getPagedScore = async ({ size, start }) => {
     const slug = getSlug();
 
-    let ref = this.db
-      .collection('users')
+    let ref = this.collection
       .where('slug', '==', slug)
       .orderBy('score', 'desc')
       .limit(size);
