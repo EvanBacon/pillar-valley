26e49a2401581ffffe8f807ee51113c4102a95fd
Evan Bacon <baconbrix@gmail.com>
2020-06-09 14:33:48 -0700

    Remove composer

--- client/src/Game/index.js
+++ client/src/Game/index.js
@@ -1,13 +1,10 @@
-import Settings from "../constants/Settings";
-import { Renderer } from "expo-three";
-import Composer from "./Composer";
 import Game from "./Game";
 
 class Machine {
   time = 0;
 
   onContextCreateAsync = async ({ gl, width, height, scale: pixelRatio }) => {
-    this.renderer = new Renderer({
+    this.renderer = new ExpoTHREE.Renderer({
       gl,
       width,
       height,
@@ -16,15 +13,6 @@ class Machine {
 
     this.game = new Game(width, height, this.renderer);
     await this.game.loadAsync();
-
-    if (Settings.isComposerEnabled) {
-      this.composer = Composer(
-        gl,
-        this.renderer,
-        this.game.scene,
-        this.game.camera
-      );
-    }
   };
 
   onTouchesBegan = (state) => this.game.onTouchesBegan(state);
@@ -33,11 +21,9 @@ class Machine {
     const { scale } = layout;
     const width = layout.width;
     const height = layout.height;
+
     if (this.renderer) {
       this.renderer.setSize(width, height);
-    }
-    if (this.composer) {
-      this.composer.setSize(width, height);
     }
     if (this.game) {
       this.game.onResize({ width, height });
@@ -46,13 +32,8 @@ class Machine {
 
   onRender = (delta) => {
     this.time += delta;
-
     this.game.update(delta, this.time);
-    if (this.composer) {
-      this.composer.render(delta);
-    } else {
-      this.renderer.render(this.game.scene, this.game.camera);
-    }
+    this.renderer.render(this.game.scene, this.game.camera);
   };
 }
 
