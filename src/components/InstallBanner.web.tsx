b8ebbac0e591ad3e1932d71a768b1d9af90dc294
evanbacon <bacon@expo.io>
2023-05-30 22:52:21 -0500

    wip -- idk

--- src/components/InstallBanner.web.tsx
+++ src/components/InstallBanner.web.tsx
@@ -19,19 +19,19 @@ export const InstallBanner = React.forwardRef(function InstallBanner(
   //   return null;
 
   // TODO: Use fully qualified URL with maximum info for rehydration (i.e. add groups)
-  const pathname = "/"; // usePathname();
+  // const pathname = "/"; // usePathname();
 
-  const url = React.useMemo(() => {
-    const url = new URL(
-      pathname,
-      typeof window === "undefined"
-        ? origin ?? "https://acme.com"
-        : window.location.href
-    );
-    // url.searchParams.set("utm_medium", "banner");
-    // url.searchParams.set("utm_content", "apple-itunes-app");
-    return url.toString();
-  }, [pathname]);
+  // const url = React.useMemo(() => {
+  //   const url = new URL(
+  //     pathname,
+  //     typeof window === "undefined"
+  //       ? origin ?? "https://acme.com"
+  //       : window.location.href
+  //   );
+  //   // url.searchParams.set("utm_medium", "banner");
+  //   // url.searchParams.set("utm_content", "apple-itunes-app");
+  //   return url.toString();
+  // }, [pathname]);
 
   if (process.env.NODE_ENV !== "production") {
     // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -48,7 +48,8 @@ export const InstallBanner = React.forwardRef(function InstallBanner(
     <meta
       ref={ref}
       name="apple-itunes-app"
-      content={`app-id=${id}, app-argument=${url}`}
+      content={`app-id=${id}`}
+      // content={`app-id=${id}, app-argument=${url}`}
     />
   );
 });
