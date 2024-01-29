959e3d85908c95c826ee798f49d9228b1dd80ea2
evanbacon <bacon@expo.io>
2024-01-29 11:47:22 -0600

    drop extra boilerplate

--- modules/expo-app-icon/index.ts
+++ modules/expo-app-icon/index.ts
@@ -1,4 +1,31 @@
-// Import the native module. On web, it will be resolved to ExpoAppIcon.web.ts
-// and on native platforms to ExpoAppIcon.ts
+// Add global types for `expo?.modules?.ExpoAppIcon`
+declare global {
+  namespace Native {
+    interface ExpoAppIcon {
+      isSupported: boolean;
+      /** Pass `null` to use the default icon. */
+      setAlternateIcon: (iconName: string | null) => Promise<string | null>;
+      /** @returns `null` if the default icon is being used. */
+      getAlternateIcon: () => Promise<string | null>;
+    }
+  }
 
-export { default } from "./src/ExpoAppIconModule";
+  var expo:
+    | {
+        modules: {
+          ExpoAppIcon?: Native.ExpoAppIcon;
+        };
+      }
+    | undefined;
+}
+
+export default (expo?.modules?.ExpoAppIcon ?? {
+  // If the native module is not available, return a mock that does nothing.
+  isSupported: false,
+  async setAlternateIcon() {
+    return null;
+  },
+  async getAlternateIcon() {
+    return null;
+  },
+}) satisfies Native.ExpoAppIcon;
--- modules/expo-app-icon/src/ExpoAppIconModule.web.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  isSupported: false,
-  async setAlternateIcon() {
-    return null;
-  },
-  getAlternateIcon() {
-    return null;
-  },
-};
